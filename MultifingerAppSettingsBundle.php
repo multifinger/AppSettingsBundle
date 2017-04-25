1e0f932b0b4311fb0d7c3fabc23c6c6a380a6159
Maksim Borisov <maksim.i.borisov@gmail.com>
2017-04-25 18:53:48 +0700

    UPD import from LKSettings (Entity, Repository, Service, Bundle Config)

---
+++ MultifingerAppSettingsBundle.php
@@ -0,0 +1,11 @@
+<?php
+namespace Multifinger\AppSettingsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Maksim Borisov <maksim.i.borisov@gmail.com> 25.04.17 17:19
+ */
+class MultifingerAppSettingsBundle extends Bundle
+{
+}
